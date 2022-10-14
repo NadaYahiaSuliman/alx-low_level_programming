739bc6dc656cb1e127d74bce5979ca8067041b43
NadaYahiaSuliman <nada.yahia.suliman@gmail.com>
2022-10-14 15:50:22 -0700

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,8 +8,9 @@
  * Return: Always 0 (Success)
  */
 int main(void)
+{
 	int n;
-	rand(time(0));
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 {
@@ -20,7 +21,8 @@ int main(void)
 	printf("%d is zero\n", n);
 }
 	else
-{
+{	
 	printf("%d is negative\n", n);
-}
+}	
 	return (0);
+}
