978f655bfe403c2a217df14783130fc5c62f5cb2
NadaYahiaSuliman <nada.yahia.suliman@gmail.com>
2022-10-27 03:35:04 -0700

    commit

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - magic
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	int n;
@@ -11,7 +16,6 @@ int main(void)
 /*
  * write your line of code here...
  * Remember:
- * main - magic
  * - you are not allowed to use a
  * - you are not allowed to modify p
  * - only one statement
